063a52a17b3fc28b3b7ea6ee89a29af212f9c26e
Florian <florianpaulschoene@gmail.com>
2025-07-28 11:14:43 +0200

    fix db and populate with data

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,10 +9,10 @@ import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: "sqlite", // <--- Specify your database type here
-      database: "./src/data/database.sqlite", // <--- Path to your DB file (for SQLite) or database name
-      entities: [Quote], 
-      synchronize: true, 
+      type: 'sqlite', // <--- Specify your database type here
+      database: './src/data/database.sqlite', // <--- Path to your DB file (for SQLite) or database name
+      entities: [Quote],
+      synchronize: true,
       logging: false, // Set to 'all' or true to see SQL queries in the console (useful for debugging)
     }),
     QuotesModule,
